82f0437a4de646dd995b4d9fd1e6f6fa00cfdc39
76574 <a765747458@gmail.com>
2021-09-09 15:00:17 +0800

    promise function* await

--- js/eloquent-javascript/chapter10模块/mode-for-browser/app.js
+++ js/eloquent-javascript/chapter10模块/mode-for-browser/app.js
@@ -1,3 +1,6 @@
 var a = require('a.js')
 var b = require('b.js')
 
+
+var x = await Promise.resolve('we')
+console.log(x)
--- js/eloquent-javascript/chapter10模块/taskqueue.js
+++ js/eloquent-javascript/chapter10模块/taskqueue.js
@@ -1,26 +1,41 @@
-class TaskQueue {
-  constructor(limit = 1) {
-      this.limit = limit
-      this.tasks = []
-      this.runningCount = 0
+// 
+    // class TaskQueue {
+    //   constructor(limit = 1) {
+    //       this.limit = limit
+    //       this.tasks = []
+    //       this.runningCount = 0
 
-      this.startNewTask = () => {
-          if (this.tasks.length) {
-              var task = this.tasks.shift()
-              task(this.startNewTask)
-          } else {
-              this.runningCount--
-          }
-      }
-  }
+    //       this.startNewTask = () => {
+    //           if (this.tasks.length) {
+    //               var task = this.tasks.shift()
+    //               task(this.startNewTask)
+    //           } else {
+    //               this.runningCount--
+    //           }
+    //       }
+    //   }
 
-  addTask(task) {
-      if (this.runningCount >= this.limit) {
-          this.tasks.push(task)
-      } else {
-          this.runningCount++
-          task(this.startNewTask)
-      }
-      return this
-  }
+    //   addTask(task) {
+    //       if (this.runningCount >= this.limit) {
+    //           this.tasks.push(task)
+    //       } else {
+    //           this.runningCount++
+    //           task(this.startNewTask)
+    //       }
+    //       return this
+    //   }
+    // }
+
+class TaskQueue1{  // promise方式
+    constructor () {
+        this.promise = Promise.resolve()
+    }
+    addTask(task) {  // 事件处理器（task 、任务）
+        this.promise = this.promise.then(() => {
+            return new Promise(task)
+            // return new Promise(resolve => {  // resolve 是下一个then的传递的函数
+            //     task(resolve)
+            // })
+        })
+    }
 }
